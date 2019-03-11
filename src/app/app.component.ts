c8c9144102d16e40d6b1580f9d062d607446a66e
Adner Ramos <adnerramos21@gmail.com>
2019-03-11 17:38:15 -0500

    Added official images to be used. Create slideshow from scratch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { showHide, slideUpDown, colorPickerParentAnimation, colorPickerAnimation, scaleUpDown } from './app.component.animation';
 
 @Component({
@@ -13,9 +13,14 @@ import { showHide, slideUpDown, colorPickerParentAnimation, colorPickerAnimation
     colorPickerAnimation
   ]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   isVisible = true;
+  slideIndex = 1;
+
+  ngAfterViewInit(): void {
+    this.showSlides(this.slideIndex);
+  }
 
   shopNow() {
     this.hideColorPicker();
@@ -28,4 +33,35 @@ export class AppComponent {
   goBack() {
     this.isVisible = true;
   }
+
+  slideUp(n: number) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  slideDown(n: number) {
+    this.showSlides(this.slideIndex -= n);
+  }
+
+  currentSlide(n: number) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n: number) {
+    const slides = document.getElementsByClassName('mySlides');
+    const dots = document.getElementsByClassName('dot');
+    const slideContainer = document.getElementsByClassName('slideshow-container') as any;
+    let slideHeight = 0,
+      calculateHeight = 0;
+
+    if (n > slides.length) { this.slideIndex = 1; }
+
+    if (n < 1) { this.slideIndex = slides.length; }
+
+    slideHeight = +window.getComputedStyle(slides[this.slideIndex - 1], null).getPropertyValue('height').slice(0, -2);
+
+    calculateHeight = -(slideHeight * (this.slideIndex - 1));
+
+    slideContainer[0].style.transform = 'translate3d(0px, ' + calculateHeight + 'px, 0px)';
+  }
+
 }
